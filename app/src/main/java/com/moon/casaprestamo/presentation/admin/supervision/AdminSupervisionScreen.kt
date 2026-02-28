749a23b3bee4e3a0766a7c6de6f83f63abbe14a4
moongie0514 <129601412+moongie0514@users.noreply.github.com>
2026-02-28 14:59:43 -0600

    Improve admin header responsiveness with icon-only add action

--- app/src/main/java/com/moon/casaprestamo/presentation/admin/supervision/AdminSupervisionScreen.kt
+++ app/src/main/java/com/moon/casaprestamo/presentation/admin/supervision/AdminSupervisionScreen.kt
@@ -8,6 +8,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
 fun AdminSupervisionScreen(
+    idAprobador: Int,
     viewModel: SupervisionViewModel = hiltViewModel(),
     modifier: Modifier = Modifier
 ) {
@@ -15,8 +16,8 @@ fun AdminSupervisionScreen(
 
     AdminSupervisionContent(
         uiState = uiState,
-        onAprobar = viewModel::aprobarPrestamo,
-        onRechazar = viewModel::rechazarPrestamo,
+        onAprobar = { idPrestamo -> viewModel.aprobarPrestamo(idPrestamo, idAprobador) },
+        onRechazar = { idPrestamo -> viewModel.rechazarPrestamo(idPrestamo, idAprobador) },
         modifier = modifier
     )
 }
